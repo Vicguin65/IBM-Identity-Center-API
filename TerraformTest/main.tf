22afe8d5de43dbf7b2c4a2be7384e597a8f608db
ArnavM34 <116925124+ArnavM34@users.noreply.github.com>
2024-06-14 15:33:42 -0400

    tf file for ec2 instance
    This is the first pull after terraform running in RCOS.

---
+++ TerraformTest/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region = "us-west-1"
+}
+
+resource "aws_instance" "my_first_instance" {
+    ami = "ami-0ca1f30768d0cf0e1"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "arnav-terraform-first"
+    }
+}
