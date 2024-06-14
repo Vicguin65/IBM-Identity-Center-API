37962e93f00586a9ab9af4c9c1f2a76b80f1a2f6
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-06-14 15:36:26 -0400

    minor bug

--- TerraformTest/main.tf
+++ TerraformTest/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_instance" "my_first_instance" {
     ami = "ami-0ca1f30768d0cf0e1"
-    instance_type = "t2.micro"
+    instance_type = "t2.xlarge"
     tags = {
         Name = "arnav-terraform-first"
     }
